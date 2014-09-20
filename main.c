deb9b6780aaadb078897f541f167acf0d5bb0cd3
gsingh62 <gsingh62@gatech.edu>
2014-09-20 19:52:59 -0400

    added blocking semaphores

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -8,6 +10,60 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
+
+/*semaphore union*/
+union semun{
+	int val;
+	struct semid_ds *buf;
+	unsigned short int * array;
+	struct seminfo *__buf;
+};
+
+/*Allocate a binary semaphore and obtain its id. If the semaphore already exists then the allocation is not required*/
+int binary_semaphore_allocation(key_t key,int sem_flags){
+	return semget(key,1,sem_flags);
+}
+
+/*Deallocate a binary semaphore. All users have finished their use. Return -1 on failure.*/
+int binary_semaphore_deallocate(int semid){
+	union semun ignored_argument;
+	return semctl(semid, 1, IPC_RMID, ignored_argument);
+}
+
+/*Initialize a binary semaphore with a value of 1*/
+int binary_semaphore_initialize(int semid){
+	union semun argument;
+	unsigned short values[1];
+	values[0]=1;
+	argument.array=values;
+	return semctl(semid,0,SETALL, argument);
+}
+
+/*Wait on a binary semaphore. Block until the semaphore value is positive, then decrement it by 1.*/
+int binary_semaphore_wait(int semid){
+	struct sembuf operations[1];
+	/*Use the first (and only) semaphore. */
+	operations[0].sem_num=0;
+	/*Decrement by 1*/
+	operations[0].sem_op=-1;
+	/*Permit undo'ing*/
+	operations[0].sem_flg = SEM_UNDO;
+	
+	return semop(semid,operations,1);
+}
+
+/*Post to a binary semaphore: increment its value by 1. This returns immediately*/
+int binary_semaphore_post(int semid){
+	struct sembuf operations[1];
+	/*Use the first (and only) semaphore. */
+	operations[0].sem_num=0;
+	/*Increment by 1*/
+	operations[0].sem_op=1;
+	/*Permit undo'ing*/
+	operations[0].sem_flg = SEM_UNDO;
+	
+	return semop(semid,operations,1);
+}
 
 int main(void) {
     
@@ -80,7 +136,7 @@ int main(void) {
         segment_size = shmbuffer.shm_segsz;
         printf("segment size %d\n",segment_size);
         /*Write a string to the shared memory segment*/
-        sprintf(shared_memory,"Hello World I am Gayatri.");
+        sprintf(shared_memory,"Hello World.");
         /*detach the shared memory segment*/
         shmdt(shared_memory);
         
